95f4a2a230647ca2dc1a047bba198246e66dd062
Igor Šarčević <igor@renderedtext.com>
2016-06-22 17:33:45 +0200

    Implement the rest of publishing

--- lib/tackle.ex
+++ lib/tackle.ex
@@ -1,10 +1,19 @@
 defmodule Tackle do
   require Logger
+  use AMQP
 
   def publish(message, options) do
     url = options[:url]
     exchange = options[:exchange]
     routing_key = options[:routing_key]
+
+    Logger.info "Connecting to '#{url}'"
+    channel = Tackle.Channel.create(url)
+
+    Logger.info "Declaring an exchange '#{exchange}'"
+    Exchange.direct(channel, exchange, durable: true)
+
+    AMQP.Basic.publish(channel, exchange, routing_key, message, persistent: true)
   end
 
 end
