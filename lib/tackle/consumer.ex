b498d373834538e4f7e1a68410f1017d6a66939a
Igor Šarčević <igor@renderedtext.com>
2016-08-07 21:16:44 +0200

    Publish to delay queue

--- lib/tackle/consumer.ex
+++ lib/tackle/consumer.ex
@@ -10,8 +10,8 @@ defmodule Tackle.Consumer do
     routing_key = options[:routing_key]
     service_name = options[:service_name]
 
-    retry_delays = options[:retry_delays] || [30, 3600]
-    retry_limits = options[:retry_limits] || [10, 10]
+    retry_delay = options[:retry_delay] || 10
+    retry_limit = options[:retry_limit] || 10
 
     quote do
       @behaviour Tackle.Consumer.Behaviour
@@ -28,23 +28,24 @@ defmodule Tackle.Consumer do
         service = unquote(service_name)
         remote_exchange = unquote(exchange)
         routing_key = unquote(routing_key)
-        retry_delays = unquote(retry_delay)
-        retry_limits = unquote(retry_limit)
+        retry_delay = unquote(retry_delay)
+        retry_limit = unquote(retry_limit)
 
         channel = Tackle.Channel.create(url)
 
         Tackle.Exchange.create_exchange_for_service(channel, service)
         Tackle.Exchange.bind(channel, service, remote_exchange, routing_key)
 
-        queue = Tackle.Queue.create_queues(channel, service, routing_key, delays)
+        queue = Tackle.Queue.create_queues(channel, service, routing_key, retry_delay)
 
         {:ok, _consumer_tag} = AMQP.Basic.consume(channel, queue)
 
         state = %{
+          url: url,
           channel: channel,
-          queue_name: queue_name,
+          service_name: service_name,
           routing_key: routing_key,
-          exchange: exchange,
+          remote_exchange: remote_exchange,
           retry_delay: retry_delay,
           retry_limit: retry_limit
         }
@@ -76,27 +77,17 @@ defmodule Tackle.Consumer do
           AMQP.Basic.ack(state.channel, tag)
         rescue
           _ ->
-            delayed_retry(state, payload, retry_count)
+            Tackle.DelayedRetry.retry(
+              state.url,
+              state.service_name,
+              state.routing_key,
+              payload,
+              headers,
+              state.retry_delays,
+              state.retry_limit
+            )
+
             AMQP.Basic.nack(state.channel, tag, [multiple: false, requeue: false])
-        end
-      end
-
-      defp delayed_retry(state, payload, retry_count)  do
-        if retry_count < state.retry_limit do
-          Logger.info "Sending message to the dead letters. Retry count: '#{retry_count}'"
-
-          dead_exchange = Tackle.Queue.dead_letter_exchange_name(state.exchange)
-
-          options = [
-            persistent: true,
-            headers: [
-              retry_count: retry_count + 1
-            ]
-          ]
-
-          AMQP.Basic.publish(state.channel, dead_exchange, state.routing_key, payload, options)
-        else
-          Logger.info "Reached #{retry_count} retries. Discarding message"
         end
       end
 
--- lib/tackle/queue.ex
+++ lib/tackle/queue.ex
@@ -2,58 +2,54 @@ defmodule Tackle.Queue do
   use AMQP
   require Logger
 
-  def create_queues(channel, exchange, routing_key, delays) do
-    create_nodelay_queue(channel, exchange, routing_key)
-
-    delays |> Enum.each(fn(delay) ->
-      create_delay_queue(channel, exchange, routing_key, delay)
-    end)
-
-    create_dead_queue(channel, exchange, routing_key)
+  def create_queues(channel, service, routing_key, delay) do
+    create_nodelay_queue(channel, service, routing_key)
+    create_delay_queue(channel, service, routing_key, delay)
+    create_dead_queue(channel, service, routing_key)
   end
 
-  def create_nodelay_queue(channel, exchange, routing_key) do
-    queue_name = nodelay_queue_name(exchange, routing_key)
+  def create_nodelay_queue(channel, service, routing_key) do
+    queue_name = nodelay_queue_name(service, routing_key)
 
     Logger.info "Creating queue '#{queue_name}'"
 
     Queue.declare(channel, queue_name, durable: true)
-    Exchange.direct(channel, exchange, durable: true)
+    Exchange.direct(channel, service, durable: true)
   end
 
-  def create_delay_queue(channel, exchange, routing_key, delay) do
-    queue_name = delay_queue_name(exchange, routing_key, delay)
+  def create_delay_queue(channel, service, routing_key, delay) do
+    queue_name = delay_queue_name(service, routing_key, delay)
 
     Logger.info "Creating queue '#{queue_name}'"
 
     Queue.declare(channel, queue_name, [
       durable: true,
       arguments: [
-        {"x-dead-letter-exchange", :longstr, exchange},
+        {"x-dead-letter-exchange", :longstr, service},
         {"x-dead-letter-routing-key", :longstr, routing_key},
         {"x-message-ttl", :long, delay * 1000}
       ]
     ])
   end
 
-  def create_dead_queue(channel, exchange, routing_key) do
-    queue_name = dead_queue_name(exchange, routing_key)
+  def create_dead_queue(channel, service, routing_key) do
+    queue_name = dead_queue_name(service, routing_key)
 
     Logger.info "Creating queue '#{queue_name}'"
 
     Queue.declare(channel, queue_name, durable: true)
   end
 
-  def nodelay_queue_name(exchange, routing_key) do
-    "#{exchange}.#{routing_key}.nodelay"
+  def nodelay_queue_name(service, routing_key) do
+    "#{service}.#{routing_key}.nodelay"
   end
 
-  def delay_queue_name(exchange, delay) do
-    "#{exchange}.#{routing_key}.delay.#{delay}"
+  def delay_queue_name(service, delay) do
+    "#{service}.#{routing_key}.delay.#{delay}"
   end
 
-  def dead_queue_name(exchange, routing_key) do
-    "#{exchange}.#{routing_key}.dead"
+  def dead_queue_name(service, routing_key) do
+    "#{service}.#{routing_key}.dead"
   end
 
 end
