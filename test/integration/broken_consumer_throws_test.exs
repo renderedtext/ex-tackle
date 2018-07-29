7c5a4038c56d7a49f4223313ac42f34e7cc673a4
Andreas Riemer <andreas.riemer@gmail.com>
2018-07-29 22:33:02 +0200

    Cleans up after broken_consumer_throws_test

--- test/integration/broken_consumer_throws_test.exs
+++ test/integration/broken_consumer_throws_test.exs
@@ -7,29 +7,35 @@ defmodule Tackle.BrokenConsumerThrowsTest do
   defmodule BrokenConsumer do
     use Tackle.Consumer,
       url: "amqp://localhost",
-      exchange: "test-exchange",
+      exchange: "ex-tackle.test-exchange",
       routing_key: "test-messages",
-      service: "broken-service-throw",
+      service: "ex-tackle.broken-service-throw",
       retry_delay: 1,
       retry_limit: 3
 
     def handle_message(message) do
       message |> MessageTrace.save("broken-service-throw")
 
-      throw {1, 3, 4}
+      throw({1, 3, 4})
     end
   end
 
   @publish_options %{
     url: "amqp://localhost",
-    exchange: "test-exchange",
-    routing_key: "test-messages",
+    exchange: "ex-tackle.test-exchange",
+    routing_key: "test-messages"
   }
 
   setup do
+    reset_test_exchanges_and_queues()
+
+    on_exit(fn ->
+      reset_test_exchanges_and_queues()
+    end)
+
     MessageTrace.clear("broken-service-throw")
 
-    {:ok, _} = BrokenConsumer.start_link
+    {:ok, _} = BrokenConsumer.start_link()
 
     :timer.sleep(1000)
   end
@@ -42,5 +48,12 @@ defmodule Tackle.BrokenConsumerThrowsTest do
 
       assert MessageTrace.content("broken-service-throw") == "Hi!Hi!Hi!Hi!"
     end
+  end
+
+  defp reset_test_exchanges_and_queues do
+    Support.delete_all_queues("ex-tackle.broken-service-throw.test-messages")
+
+    Support.delete_exchange("ex-tackle.broken-service-throw.test-messages")
+    Support.delete_exchange("ex-tackle.test-exchange")
   end
 end
