91d02e645c6395292628c4e33acd27b1656c5a88
Andreas Riemer <andreas.riemer@gmail.com>
2018-07-29 22:55:46 +0200

    Cleans up after prefetch_count_2_test

--- test/integration/prefetch_count_2_test.exs
+++ test/integration/prefetch_count_2_test.exs
@@ -4,34 +4,38 @@ defmodule Tackle.ParallelMessageHandling_2_Test do
   defmodule TestConsumer do
     use Tackle.Consumer,
       url: "amqp://localhost",
-      exchange: "test-prefetch-2-exchange",
+      exchange: "ex-tackle.test-prefetch-2-exchange",
       routing_key: "prefetch",
-      service: "prefetch-count-service",
+      service: "ex-tackle.prefetch-count-service",
       prefetch_count: 2
 
     def handle_message(message) do
       "#PID" <> spid = message
-      sup = spid |> String.to_char_list() |> :erlang.list_to_pid()
+      sup = spid |> String.to_charlist() |> :erlang.list_to_pid()
       Task.Supervisor.async_nolink(sup, fn -> :timer.sleep(:infinity) end)
 
       receive do
-        msg -> nil
+        _msg -> nil
       end
     end
   end
 
   @publish_options %{
     url: "amqp://localhost",
-    exchange: "test-prefetch-2-exchange",
+    exchange: "ex-tackle.test-prefetch-2-exchange",
     routing_key: "prefetch"
   }
 
   setup do
+    reset_test_exchanges_and_queues()
+
+    on_exit(fn ->
+      reset_test_exchanges_and_queues()
+    end)
+
     {:ok, _} = TestConsumer.start_link()
 
-    {:ok, sup} =
-      Task.Supervisor.start_link()
-      |> IO.inspect()
+    {:ok, sup} = Task.Supervisor.start_link()
 
     :timer.sleep(1000)
 
@@ -64,5 +68,12 @@ defmodule Tackle.ParallelMessageHandling_2_Test do
 
       assert Task.Supervisor.children(sup) |> Enum.count() == 0
     end
+  end
+
+  defp reset_test_exchanges_and_queues do
+    Support.delete_all_queues("ex-tackle.prefetch-count-service.prefetch", 10)
+
+    Support.delete_exchange("ex-tackle.prefetch-count-service.prefetch")
+    Support.delete_exchange("ex-tackle.test-prefetch-2-exchange")
   end
 end
