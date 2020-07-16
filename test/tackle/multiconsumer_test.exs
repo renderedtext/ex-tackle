352f83deba4ec18571fe629893b47f9aafbb7e26
bmarkons <mamaveb@gmail.com>
2020-07-16 11:46:47 +0200

    Fix multiconsumer modules naming
    The name of individual consumer takes caller module into account

--- test/tackle/multiconsumer_test.exs
+++ test/tackle/multiconsumer_test.exs
@@ -27,6 +27,20 @@ defmodule Tackle.MulticonsumerTest do
     end
   end
 
+  test "inspect modules" do
+    defined_consumer_modules = :code.all_loaded()
+                       |> Enum.map(fn {mod, _} -> mod end)
+                       |> Enum.filter(fn module -> String.contains?(Atom.to_string(module), "exchange-1.key1") end)
+                       |> Enum.sort()
+
+    expected_consumer_modules =
+      [:"Elixir.Tackle.MulticonsumerTest.MulticonsumerExampleBeta.exchange-1.key1",
+       :"Elixir.Tackle.MulticonsumerTest.MulticonsumerExample.exchange-1.key1"]
+       |> Enum.sort()
+
+    assert defined_consumer_modules == expected_consumer_modules
+  end
+
   test "successfully starts multiconsumers" do
     import Supervisor.Spec
     opts = [strategy: :one_for_one, name: Front.Supervisor]
