941b71b085cfe50c34763f06878fa8e56bb19df3
Lucas Pinheiro <lpinheiro@renderedtext.com>
2021-11-22 20:11:02 -0300

    Remove unused deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tackle.Mixfile do
     [
       app: :tackle,
       version: "0.2.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
