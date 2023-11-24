b931667f1135d2f54e8a368794b213629edab9e0
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2023-11-24 12:08:30 +0100

    bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tackle.Mixfile do
   def project do
     [
       app: :tackle,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
