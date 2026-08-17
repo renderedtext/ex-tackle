ee9949b39172ac12defb6a7c60574c021d3e59c1
Marcos Filipe <mfilipe@renderedtext.com>
2026-08-17 08:40:56 -0300

    chore: bump ex-tackle to v0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tackle.Mixfile do
   def project do
     [
       app: :tackle,
-      version: "0.3.0",
+      version: "0.4.1",
       elixir: "~> 1.13",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
